0e15304bb08a529c8470841401a92136d171d42c
Alexander A. Kropotin <ololx@icloud.com>
2021-10-31 20:22:28 +0500

    feature(*) init project

---
+++ quick-blank-file/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  quick-blank-file
+//
+//  Created by Alexander A. Kropotin on 31/10/2021.
+//  Copyright © 2021 Alexander A. Kropotin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ quick-blank-file/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  quick-blank-file
+//
+//  Created by Alexander A. Kropotin on 31/10/2021.
+//  Copyright © 2021 Alexander A. Kropotin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
